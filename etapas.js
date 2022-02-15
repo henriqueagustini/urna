b9aeee894d2f9e46e7f0b2b0935399b8b67a48e5
Henrique Agustini <henrique80@gmail.com>
2022-02-15 01:23:20 -0300

    Projeto urna eletrônica

---
+++ etapas.js
@@ -0,0 +1,53 @@
+let etapas = [
+    {
+        titulo: "VEREADOR",
+        numeros: 5,
+        candidatos: [
+            {
+                numero: "38111",
+                nome: "Fulano de tal",
+                partido: "ABC",
+                fotos:[
+                    {url:"38111.png", legenda: "Vereador"}
+                ]
+            },
+            {
+                numero: "77222",
+                nome: "Beltrano de tal",
+                partido: "DEFG",
+                fotos:[
+                    {url:"77222.png", legenda: "Vereador"}
+                ]
+            },
+        ]
+
+    },
+    {
+        titulo: "PREFEITO",
+        numeros: "2",
+        candidatos: [
+            {
+                numero: "10",
+                nome: "Fulano de tal",
+                partido: "ABC",
+                vice: "Cic",
+                fotos:[
+                    {url:"10.png", legenda: "Prefeito"},
+                    {url:"9.png", legenda: "Vice-Prefeito", small:true}
+                ]
+            },
+            {
+                numero: "11",
+                nome: "Beltrano de tal",
+                partido: "DEFG",
+                vice: "Manolo",
+                fotos:[
+                    {url:"11.png", legenda: "Prefeito"},
+                    {url:"8.png", legenda: "Vice-Prefeito", small:true}
+                ]
+            },
+        ]
+
+    },
+
+]
